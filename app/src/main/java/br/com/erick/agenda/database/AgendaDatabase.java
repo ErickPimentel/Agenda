594baa8330f4814bf5fa3d1c53f1fb84bb69704d
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-29 00:03:02 -0300

    Refatoracao ConversorCalendar, AgendaDatabase, Aluno, ListaAlunosAdapter e criacao da classe AgendaMigrations

--- app/src/main/java/br/com/erick/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/erick/agenda/database/AgendaDatabase.java
@@ -1,5 +1,7 @@
 package br.com.erick.agenda.database;
 
+import static br.com.erick.agenda.database.AgendaMigrations.TODAS_MIGRATIONS;
+
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -25,32 +27,7 @@ public abstract class AgendaDatabase extends RoomDatabase {
     public static AgendaDatabase getInstance(Context context) {
         return Room
                 .databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
-                .addMigrations(new Migration(1, 2) {
-                                   @Override
-                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
-                                       database.execSQL("ALTER TABLE Aluno ADD COLUMN sobrenome TEXT");
-                                   }
-                               }, new Migration(2, 3) {
-                                   @Override
-                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
-                                       //Criar nova tabela com as informacoes desejadas
-                                       database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `nome` TEXT, `telefone` TEXT, `email` TEXT)");
-
-                                       //Copiar dados da tabela antiga para a nova
-                                       database.execSQL("INSERT INTO Aluno_novo (id, nome, telefone, email) SELECT id, nome, telefone, email FROM Aluno");
-
-                                       //remove tabela antiga
-                                       database.execSQL("DROP TABLE Aluno");
-
-                                       //Renomear a tabela nova com o nome da tabela antiga
-                                       database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
-                                   }
-                               }, new Migration(3, 4) {
-                                   @Override
-                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
-                                       database.execSQL("ALTER TABLE Aluno ADD COLUMN momentoDeCadastro INTEGER");
-                                   }
-                               }
-                ).allowMainThreadQueries().build();
+                .addMigrations(TODAS_MIGRATIONS)
+                .allowMainThreadQueries().build();
     }
 }
